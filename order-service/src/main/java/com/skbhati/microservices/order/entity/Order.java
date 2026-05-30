9393b71334e4e972d4208b813ea241186323ed7f
Sonu Kumar <sonurust@gmail.com>
2026-05-30 23:21:54 +0530

    set up MySQL database with Docker, add MySQL connector dependency, and configure application properties

--- order-service/src/main/java/com/skbhati/microservices/order/entity/Order.java
+++ order-service/src/main/java/com/skbhati/microservices/order/entity/Order.java
@@ -1,4 +1,16 @@
 package com.skbhati.microservices.order.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity(name = "t_orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
 }
